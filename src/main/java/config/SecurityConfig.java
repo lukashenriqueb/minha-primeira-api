2f6671c92809d07e1ea979961b8fafb771d8f27e
Lukas Henrique <lukashenriqueb0@gmail.com>
2025-10-28 21:17:23 -0300

    feat: Configura regras de autorização

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package br.com.estudos.minha_primeira_api.config.SecurityConfig;
+package br.com.estudos.minha_primeira_api.config;
 
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,16 +13,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Configuration
 public class SecurityConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Bean
     public PasswordEncoder passwordEncoder() {
+        log.info(">>>> Criando o PasswordEncoder Bean (BCrypt)...");
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        log.info(">>>> Criando InMemoryUserDetailsManager Bean...");
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin123"))
@@ -40,18 +47,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info(">>>> Configurando o SegurityFilterChain...");
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/produtos/**", "/categorias/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/produtos", "/produtos/**", "/categorias/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/produtos").authenticated()
-                        .requestMatchers(HttpMethod.PUT, "/produtos/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/produtos/**").hasAnyRole("ADMIN")
-                        .requestMatchers("/categorias/**").hasAnyRole("ADMIN")
-
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+                                .requestMatchers(HttpMethod.PUT, "/produtos/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/produtos/**").hasRole("ADMIN")
+                                .requestMatchers("/categorias/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                        )
+                .httpBasic(withDefaults())
+        .csrf(csrf -> csrf.disable())
+        .formLogin(form -> form.disable());
         return http.build();
     }
-
 }
