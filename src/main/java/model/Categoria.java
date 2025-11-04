7b3a3d2a0393916996018b82449c39a9e6878b54
Lukas Henrique <lukashenriqueb0@gmail.com>
2025-11-03 22:29:10 -0300

    removi o loop infinito

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -1,18 +1,30 @@
 package br.com.estudos.minha_primeira_api.model;
+import br.com.estudos.minha_primeira_api.model.Produto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
 
 @Entity
 public class Categoria {
 
+    public Long getId;
     public String getNome;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    @JsonIgnore
+    private List<Produto> produtos;
+
+
 
     public Categoria() {
     }
